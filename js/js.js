fb067091de50f7041aa7cd82f4f039ac62e835ed
AbhiDev777 <corp.java11@gmail.com>
2025-01-25 10:07:07 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,10 @@
+
+/*window.onload = function() {
+    const image = document.getElementById('image');
+    
+    // Make the image disappear after 1 second
+    setTimeout(() => {
+        image.style.display = 'none'; 
+    }, 5000); // 1000ms = 1 second
+};
+*/
